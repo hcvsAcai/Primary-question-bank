870867b5a36cb8826e2e1976214ee8af8f12293f
hcvsAcai <48270488+hcvsAcai@users.noreply.github.com>
2019-03-06 17:41:02 +0800

    Update a164 許願.cpp

--- a164 許願.cpp
+++ a164 許願.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int main() {  
-cout << "a003:許願	通過比率'93%'" << endl;
-	cout << " ^_^ " << endl;
+int main() 
+ {  
+        cout << "a003:許願	通過比率'93%'" << endl;
+	cout << " ^_^ " << endl;  
 	cout << "\"o\"" << endl;
 	cout << "\\^o^/";
 	
-}
+ }
