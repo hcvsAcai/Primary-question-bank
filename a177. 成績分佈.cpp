40523094f1527a94ee4dfe2c69263379e6ee6c36
hcvsAcai <48270488+hcvsAcai@users.noreply.github.com>
2019-03-06 17:55:28 +0800

    Create a177. 成績分佈.cpp

---
+++ a177. 成績分佈.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int N;
+  cin>>N ;
+  if (0<=N&&N<=100)
+{
+  if(N>=55&&N<=65)
+  cout<<"YES";
+  else
+  cout<<"NO";
+}
+  else{
+  cout<<"Error";
+}
+
+}
