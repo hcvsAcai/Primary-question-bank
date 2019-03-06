f7f4c5300b7897e4e85b664df216bb2e2342a741
hcvsAcai <48270488+hcvsAcai@users.noreply.github.com>
2019-03-06 18:06:48 +0800

    Create a181. 成績等第.cpp

---
+++ a181. 成績等第.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int  N (0<=N<=100) ;
+  cin>>N;
+  if(N>=90&&N<=100)
+  cout<<"A";
+  if(N>=80&&N<90)
+  cout<<"B";
+  if(N>=70&&N<80)
+  cout<<"C";
+  if(N>=60&&N<70)
+  cout<<"D";
+  if(N<60)
+  cout<<"E";
+}
