2180ee956884c529ab2729c9594463fdb8412878
hcvsAcai <48270488+hcvsAcai@users.noreply.github.com>
2019-03-06 18:26:29 +0800

    Create a190: 數字倒轉.cpp

---
+++ a190: 數字倒轉.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int n,s;
+  cin>>n;
+  s = 0;  
+  while( n != 0 )  
+  {                      
+    s = s * 10 + ( n%10 );     
+    n /= 10;                 
+  }  
+  cout<< s;
+}
