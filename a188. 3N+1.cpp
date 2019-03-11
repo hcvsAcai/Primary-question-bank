b0f0322693a5e94c7e7ae838e02c8c25783fcfdf
hcvsAcai <48270488+hcvsAcai@users.noreply.github.com>
2019-03-11 22:43:03 +0800

    Update a188. 3N+1.cpp

--- a188. 3N+1.cpp
+++ a188. 3N+1.cpp
@@ -1,17 +1,15 @@
-#include <iostream>
+##include <iostream>
 
 using namespace std;
 
 int main()
 {
-  int n,cnt;
+  int n,a=1;
   cin>>n;
-  cnt=1;
-  while( n != 1 )  
-{  
-   if( n%2 == 0 ) n/=2;     
-   else n = n*3+1;           
-   ++cnt ;              
-}  
-  cout<<cnt;
+  while(n!=1)
+  {
+	  (n%2==0)?n=n/2:n=n*3+1;
+    a++;
+  }
+   	cout<<a;
 }
