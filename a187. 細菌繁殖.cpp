60d24f01e2b445a9241fa77fe975fc9ced65424a
hcvsAcai <48270488+hcvsAcai@users.noreply.github.com>
2019-03-13 18:46:20 +0800

    Update a187. 細菌繁殖.cpp

--- a187. 細菌繁殖.cpp
+++ a187. 細菌繁殖.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 int main()
 {
-    float N,M,c=0;
+    float N,M,c=0;//設定一個c代表迴圈重複的次數
     cin>>N>>M;
-    while(N<M)  
+    while(N<M)  //當N<M進入迴圈
 {  
-    ++c ;   
+    c++ ; //c的數值加一  
     N*= 2.5;    
 }  
     cout<<c;
