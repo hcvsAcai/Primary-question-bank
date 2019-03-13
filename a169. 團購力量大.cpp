8ee7fb16868346ba517da9fa6bab3c2aa5348b17
hcvsAcai <48270488+hcvsAcai@users.noreply.github.com>
2019-03-13 18:03:13 +0800

    Update a169. 團購力量大.cpp

--- a169. 團購力量大.cpp
+++ a169. 團購力量大.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-  int n;
-  cin >>n;
-  cout << 100*(n-n/11) << endl;
+  int n;//設定n(變數)
+  cin >>n;//輸入n的數值
+  cout << 100*(n-n/11) << endl;//輸出 100 X ( n - n / 11 ) 的數值
 }
