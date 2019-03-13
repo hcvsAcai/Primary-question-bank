c49cceff8989a8b441e26032004bfad12ba2cfb7
hcvsAcai <48270488+hcvsAcai@users.noreply.github.com>
2019-03-13 18:28:16 +0800

    Update a178. 貨比三家.cpp

--- a178. 貨比三家.cpp
+++ a178. 貨比三家.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 int main()
 {
-  int a ,b, c ,d ;
+  int a ,b, c ,d ;//設定一個變數d(儲存結果)
   cin>>a>>b>>c;
-  d=a;
-  if(b<d) 
+  d=a;//將a的數值儲存至d
+  if(b<d)//如果條件成立將執行把b的數值儲存至d
   d=b;
-  if(c<d)  
+  if(c<d)//如果條件成立將執行把c的數值儲存至d
   d=c;
-  cout<<d;
+  cout<<d;//輸出d最後的數值
 }
