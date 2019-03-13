cf21e4866436cb9826fb1f7003a47c208491c58e
hcvsAcai <48270488+hcvsAcai@users.noreply.github.com>
2019-03-13 13:47:23 +0800

    Update a198. 找最大值.cpp

--- a198. 找最大值.cpp
+++ a198. 找最大值.cpp
@@ -4,15 +4,15 @@ using namespace std;
  
 int main()
 {
-    int n,a[10],i;
+	int n,a[10],i;
 	cin>>n;
 	for(i=0;i<n;++i)
-	cin>>a[i];
+		cin>>a[i];
 	int p=0,m=a[0];
 	for(i=1;i<n;++i)
-	if(a[i]>m)
-	{
-		m=a[i]; p=i;
-	}
+		if(a[i]>m)
+		{
+			m=a[i]; p=i;
+		}
 	cout<<p+1<<" "<<m<<endl;
 }
