517fe95bd59d83e02cc152eb8730dc023e784af9
hcvsAcai <48270488+hcvsAcai@users.noreply.github.com>
2019-03-13 18:18:56 +0800

    Update a176. 奇數與偶數.cpp

--- a176. 奇數與偶數.cpp
+++ a176. 奇數與偶數.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 int main()
 {
-  int N;
+  int N;//設定一個N(整數)
   cin>>N;
-  N%2==0?cout <<"EVEN":cout<<"ODD";
+  N%2==0?cout <<"EVEN":cout<<"ODD";//判斷N是否 / 2 之後的餘數是0
   cout<<endl;
 }
