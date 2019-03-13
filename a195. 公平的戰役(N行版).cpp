54fd07f665d68e196a05bad58082a8e695811aa1
hcvsAcai <48270488+hcvsAcai@users.noreply.github.com>
2019-03-13 20:11:45 +0800

    Update a195. 公平的戰役(N行版).cpp

--- a195. 公平的戰役(N行版).cpp
+++ a195. 公平的戰役(N行版).cpp
@@ -6,15 +6,15 @@ int main()
 {
    int n,A,B;
    cin>>n;
-while(n--)
-  {
-    cin >> A >> B;
-    if(A>B)
-    cout<<"A"<<" "<<A-B<<endl;
-    if(A<B)
-    cout<<"B"<<" "<<B-A<<endl;
-    if(A==B)
-    cout<<"0"<<endl;
-  }
+   while(n--)//讓迴圈固定跑n次
+    {
+    	cin >> A >> B;
+    	if(A>B)
+    	cout<<"A"<<" "<<A-B<<endl;
+    	if(A<B)
+    	cout<<"B"<<" "<<B-A<<endl;
+    	if(A==B)
+    	cout<<"0"<<endl;
+    }
 	
 }
