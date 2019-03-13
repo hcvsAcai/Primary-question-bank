bb60ffa094bee84aff90017291ad2e9b4e0ba723
hcvsAcai <48270488+hcvsAcai@users.noreply.github.com>
2019-03-13 18:00:12 +0800

    Update a168. 時間換算(二).cpp

--- a168. 時間換算(二).cpp
+++ a168. 時間換算(二).cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-  int h;
-  cin>>h;
-  cout << h/24 <<" " <<h%24 << endl; 
+  int h;//設定h(小時)
+  cin>>h;//輸入h(小時)
+  cout << h/24 <<" " <<h%24 << endl;//分別輸出h(小時) / 24 的數值跟h(小時) / 24的餘數
 }
