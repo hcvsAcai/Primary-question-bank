467d2783610cd890601132cf4d8cd5fe306fa59e
hcvsAcai <48270488+hcvsAcai@users.noreply.github.com>
2019-03-13 18:22:16 +0800

    Update a177. 成績分佈.cpp

--- a177. 成績分佈.cpp
+++ a177. 成績分佈.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main()
 {
-  int N;
+  int N;//設定一個N(整數)
   cin>>N ;
-  if (0<=N&&N<=100)
+  if (0<=N&&N<=100)//判斷N是否 > = 0 和 < = 100
 {
   if(N>=55&&N<=65)
   cout<<"YES";
