012551747a22caa56dcb91d52d3732828751c6ab
hcvsAcai <48270488+hcvsAcai@users.noreply.github.com>
2019-03-13 20:16:40 +0800

    Update a198. 找最大值.cpp

--- a198. 找最大值.cpp
+++ a198. 找最大值.cpp
@@ -7,11 +7,11 @@ int main()
 	cin>>n;
 	for(i=0;i<n;++i)//將i歸零(陣列從0開始用)，條件為i小於n，此迴圈只會跑到(n-1)，每次迴圈都將i加一。
 		cin>>a[i];//輸入一個索引值為i為的a陣列。
-	int p=0,m=a[0];
-	for(i=1;i<n;++i)
-		if(a[i]>m)
+	int p=0,m=a[0];//設p=0, m=a[0]為最大
+	for(i=1;i<n;++i)//再由a[1]~a[n-1]找最大
+		if(a[i]>m)//若比m大，則換成a[i]，並記住位置
 		{
 			m=a[i]; p=i;
 		}
-	cout<<p+1<<" "<<m<<endl;
+	cout<<p+1<<" "<<m<<endl;//印出位置p+1及最大值m
 }
