f1d032527bbbe4900afc323d0cacfdf30e87df8c
hcvsAcai <48270488+hcvsAcai@users.noreply.github.com>
2019-03-13 20:14:00 +0800

    Update a196. 公平的戰役(0尾版).cpp

--- a196. 公平的戰役(0尾版).cpp
+++ a196. 公平的戰役(0尾版).cpp
@@ -6,15 +6,15 @@ int main()
 {
     int n;
     int A, B;
-while(1)
-   {                
-    cin >> A >> B;
-    if(A==0&&B==0) break; 
-    if(A>B)
-    cout<<"A"<<" "<<A-B<<endl;
-    if(A<B)
-    cout<<"B"<<" "<<B-A<<endl;
-    if(A==B)
-    cout<<"0"<<endl;
+    while(1) //1代表的是「一定符合」，所以寫while(1)。其實把1換成任何數字都可以，只有0是代表「一定不符合」
+    {                
+        cin >> A >> B;
+        if(A==0&&B==0) break;//如果A和B皆為0，直接離開迴圈
+        if(A>B)
+            cout<<"A"<<" "<<A-B<<endl;
+        if(A<B)
+            cout<<"B"<<" "<<B-A<<endl;
+        if(A==B)
+            cout<<"0"<<endl;
    }
 }
