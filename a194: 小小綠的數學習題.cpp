cb6f65507b0b89b1d6f374488b37335c85ddb2f4
hcvsAcai <48270488+hcvsAcai@users.noreply.github.com>
2019-03-13 19:01:31 +0800

    Update a194: 小小綠的數學習題.cpp

--- a194: 小小綠的數學習題.cpp
+++ a194: 小小綠的數學習題.cpp
@@ -5,14 +5,12 @@ using namespace std;
 int main(  )
 {
   int a,b;
-
-  char c ;
-
+  char c ;//設定一個c(字元)
   cin>>a>>c>>b;
-if(c=='+')
+  if(c=='+')//如果c輸入一個"+"則進行a+b
     cout<<a+b<<endl;
-if(c=='-')
+  if(c=='-')//如果c輸入一個"-"則進行a-b
     cout<<a-b<<endl;
-if(c=='*')
+  if(c=='*')//如果c輸入一個"*"則進行a*b
     cout<<a*b<<endl;
  }
