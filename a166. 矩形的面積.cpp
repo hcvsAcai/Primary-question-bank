3b0135154c61fe251d24c8732dbd0f02b80f5f9c
hcvsAcai <48270488+hcvsAcai@users.noreply.github.com>
2019-03-13 17:54:21 +0800

    Update a166. 矩形的面積.cpp

--- a166. 矩形的面積.cpp
+++ a166. 矩形的面積.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
  {
-   int a,b;
-   cin>>a>>b;
-   cout<<a*b<<endl;
+   int a,b;//分別設定a(長),b(寬)
+   cin>>a>>b;//分別輸入a(長),b(寬)的數值
+   cout<<a*b<<endl;//輸出a(長) X b(寬)的數值
 }
