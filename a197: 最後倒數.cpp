8696c1bcffcc416de4991c627f8a11ca39fdbb23
hcvsAcai <48270488+hcvsAcai@users.noreply.github.com>
2019-03-13 13:44:18 +0800

    Update a197: 最後倒數.cpp

--- a197: 最後倒數.cpp
+++ a197: 最後倒數.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
-
 using namespace std;
  
 int main()
 {
-   int n,a[10],i;
-	cin>>n;
-	for(i=0;i<n;++i)
-	cin>>a[i];
-	for(i=n-1;i>=0;--i)
-	cout<<a[i]<<" ";
-	cout<<endl; 
+	int n,a[10],i;//宣告一個a陣列，元素量為10。
+	cin>>n;//輸入一個n(輸入次數)
+	for(i=0;i<n;i++)//將i歸零(陣列從0開始用)，條件為i小於n，此迴圈只會跑到(n-1)，每次迴圈都將i加一。
+		cin>>a[i];//輸入一個索引值為i為的a陣列。
+	for(i=n-1;i>=0;--i)//將i設為n-1(a陣列可用的最大索引值)，i>=0將i跑到0為止，0為a陣列可用的最小索引值。
+		cout<<a[i]<<" ";//輸出a陣列，索引值i的東西然後空格。
+	cout<<endl;//endl是換行。
 }
