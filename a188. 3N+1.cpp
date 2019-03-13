f4c6349ce9da3b5133406679a66f7d524c104986
hcvsAcai <48270488+hcvsAcai@users.noreply.github.com>
2019-03-13 18:49:43 +0800

    Update a188. 3N+1.cpp

--- a188. 3N+1.cpp
+++ a188. 3N+1.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 int main()
 {
-  int n,a=1;
+  int n,a=1;//a的初始值為一
   cin>>n;
-  while(n!=1)
-  {
-	  (n%2==0)?n=n/2:n=n*3+1;
-    a++;
+  while(n!=1)//n不等於1時進入迴圈
+  {	
+	(n%2==0)?n=n/2:n=n*3+1;//判斷是否n除2的餘數為0
+   	a++;//a加1
   }
-   	cout<<a;
+   cout<<a;
 }
