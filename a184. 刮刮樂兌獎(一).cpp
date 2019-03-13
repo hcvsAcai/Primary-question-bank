d02bfabf695d458b007386d6fadef817fa0fcf47
hcvsAcai <48270488+hcvsAcai@users.noreply.github.com>
2019-03-13 18:38:34 +0800

    Update a184. 刮刮樂兌獎(一).cpp

--- a184. 刮刮樂兌獎(一).cpp
+++ a184. 刮刮樂兌獎(一).cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 int main()
 {
-   int a,b,c,g=0;
+   int a,b,c,g=0;//設定一個儲存數值的變數，並歸0
    cin>>a;
-   b=a%10;
-   c=a%100/10;
-   if(b%3==2) g=200;
-   else if(c%2==1) g=100;
-   else if(c==b) g=50;
+   b=a%10;//經過運算後b存進a的個位數
+   c=a%100/10;//經過運算後c存進a的十位數
+   if(b%3==2) g=200;//達成條件g存進200
+   else if(c%2==1) g=100;//達成條件g存進100
+   else if(c==b) g=50;//達成條件g存進50
    cout<<g;
 }
