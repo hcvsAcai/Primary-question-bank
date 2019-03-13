7eb64b5b27b2daf8e3279e781c2564e07dbebac7
hcvsAcai <48270488+hcvsAcai@users.noreply.github.com>
2019-03-13 18:54:05 +0800

    Update a189: 所有位數和.cpp

--- a189: 所有位數和.cpp
+++ a189: 所有位數和.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 int main()
 {
-  int n,s;
+  int n,s;//設定一個s儲存當時運算的數值
   cin>>n;
   s=0;
-  while( n != 0 )
+  while( n != 0 )//當n不等於0時進入迴圈
  {  
-    s += ( n%10 );   
+    s += ( n%10 );   //將s存進n除10之後的結果
     n /= 10;                   
   }  
   cout<<s;
