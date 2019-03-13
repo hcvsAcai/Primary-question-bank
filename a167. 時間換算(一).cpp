3d7c19c6d809ba7e16b3f43c1486605d447468bd
hcvsAcai <48270488+hcvsAcai@users.noreply.github.com>
2019-03-13 18:00:30 +0800

    Update a167. 時間換算(一).cpp

--- a167. 時間換算(一).cpp
+++ a167. 時間換算(一).cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-   int d,h;//分別設定d(天),h(小時)的數值
+   int d,h;//分別設定d(天),h(小時)
    cin >> d >>h;//分別輸入d(天),h(小時)的數值
    cout << d*24+h << endl;  //輸出d(天) X 24 + h(小時)的數值
 }
