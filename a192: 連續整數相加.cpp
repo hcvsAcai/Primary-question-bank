c2a5417063d1af8f6112490824575b160c75f6a7
hcvsAcai <48270488+hcvsAcai@users.noreply.github.com>
2019-03-13 20:09:05 +0800

    Update a192: 連續整數相加.cpp

--- a192: 連續整數相加.cpp
+++ a192: 連續整數相加.cpp
@@ -5,24 +5,24 @@ main ()
   int a,b,c,i;
   cin>>a>>b;
   c=0;
-if(a>b)
+  if(a>b)
  {
-  for( i=a;i>b;i--)
- {
-    cout<<i<<"+";
-    c+=i;
+    for( i=a;i>b;i--)//設i的初始職位a，比較值為i>b，迴圈做一次i就減一
+    {
+      cout<<i<<"+";
+      c+=i;//將c + i的值存入c
+    }
+   cout<<b<<"="<<c+b;
  }
+  if(a<b)
+ {
+    for( i=a;i<b;i++)
+      {
+        cout<<i<<"+";
+        c+=i;
+      }
     cout<<b<<"="<<c+b;
  }
-if(a<b)
- {
-  for( i=a;i<b;i++)
- {
-    cout<<i<<"+";
-    c+=i;
- }
-    cout<<b<<"="<<c+b;
- }
-if(a==b)
+  if(a==b)
     cout<<a<<"="<<b;
 }
