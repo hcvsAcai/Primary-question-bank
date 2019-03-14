a8cb094df37cc8d4a27853e683048cc3fceeddf5
hcvsAcai <48270488+hcvsAcai@users.noreply.github.com>
2019-03-14 12:36:08 +0800

    Update a177. 成績分佈.cpp

--- a177. 成績分佈.cpp
+++ a177. 成績分佈.cpp
@@ -6,15 +6,17 @@ int main()
 {
   int N;//設定一個N(整數)
   cin>>N ;
+  /*判斷N所在的範圍，並輸出判定結果*/
   if (0<=N&&N<=100)//判斷N是否 > = 0 和 < = 100
-{
+  {
   if(N>=55&&N<=65)
-  cout<<"YES";
+    cout<<"YES";
   else
-  cout<<"NO";
-}
-  else{
-  cout<<"Error";
-}
+    cout<<"NO";
+  }
+  else
+  {
+    cout<<"Error";
+  }
 
 }
