96b3de55e896bc77a404b274f0e843e780887a3c
hcvsAcai <48270488+hcvsAcai@users.noreply.github.com>
2019-03-14 12:46:31 +0800

    Update a181. 成績等第.cpp

--- a181. 成績等第.cpp
+++ a181. 成績等第.cpp
@@ -6,14 +6,15 @@ int main()
 {
   int  N (0<=N<=100) ;
   cin>>N;
+  /*判斷N的所在範圍，並輸出對應級別*/
   if(N>=90&&N<=100)
-  cout<<"A";
+    cout<<"A";
   if(N>=80&&N<90)
-  cout<<"B";
+    cout<<"B";
   if(N>=70&&N<80)
-  cout<<"C";
+    cout<<"C";
   if(N>=60&&N<70)
-  cout<<"D";
+    cout<<"D";
   if(N<60)
-  cout<<"E";
+    cout<<"E";
 }
