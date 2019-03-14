a04e271a2060fb2a15e001be84ab401106b69b9c
hcvsAcai <48270488+hcvsAcai@users.noreply.github.com>
2019-03-14 12:40:50 +0800

    Update a179. 大小關係.cpp

--- a179. 大小關係.cpp
+++ a179. 大小關係.cpp
@@ -6,10 +6,11 @@ int main()
 {
   int A,B;
   cin>>A>>B;
+  /*判斷A B大小關係，並輸出A B之關係式*/
   if(A<B)
-  cout<<A<<"<"<<B;
+    cout<<A<<"<"<<B;
   if(A>B)
-  cout<<A<<">"<<B;
+    cout<<A<<">"<<B;
   if(A==B)
-  cout<<A<<"="<<B;
+    cout<<A<<"="<<B;
 }
