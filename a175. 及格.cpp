3acb2bb7b738c40aff93c790d21324b86fc24942
hcvsAcai <48270488+hcvsAcai@users.noreply.github.com>
2019-03-14 12:30:03 +0800

    Update a175. 及格.cpp

--- a175. 及格.cpp
+++ a175. 及格.cpp
@@ -4,6 +4,6 @@ int main()
 {
     int N;//設定一個N(整數)
     cin>>N;
-    N>=60?cout<<"PASS":cout<<"FAIL";//判斷N是否 > 或 = 60
+    N>=60?cout<<"PASS":cout<<"FAIL";//判斷N是否 > 或 = 60，並輸出結果
     cout<<endl;  
 }
